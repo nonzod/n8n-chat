24cfaa2e6d286666ce1b8253253e29bc64678f42
Nicola Tomassoni <n.tomassoni@tourtools.it>
2025-04-11 12:59:12 +0200

    first commit

---
+++ src/components/GetStartedFooter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import PoweredBy from '@n8n/chat/components/PoweredBy.vue';
+import { useI18n } from '@n8n/chat/composables';
+
+const { t, te } = useI18n();
+</script>
+<template>
+	<div class="chat-get-started-footer">
+		<div v-if="te('footer')">
+			{{ t('footer') }}
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+.chat-get-started-footer {
+	padding: var(--chat--spacing);
+}
+</style>
